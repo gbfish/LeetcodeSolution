85e8243a3d3e8cd784a5fcb8067d471b02e3fbeb
David Yu <equalbean@gmail.com>
2018-01-26 13:38:42 -0800

    finish 27

--- LeetcodeSolution/ViewController.swift
+++ LeetcodeSolution/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         
-//        let solution = Solution18()
+//        let solution = Solution27()
 //        solution.run()
     }
 }
