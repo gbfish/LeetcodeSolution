a247051ab6b17ac254ab9c55428afb20ca5ea628
David Yu <equalbean@gmail.com>
2018-02-01 16:41:39 -0800

    finish 83

---
+++ LeetcodeSolution/ListNode/083. Remove Duplicates from Sorted List.swift
@@ -0,0 +1,34 @@
+/*
+ 83. Remove Duplicates from Sorted List
+ 
+ Given a sorted linked list, delete all duplicates such that each element appear only once.
+ 
+ For example,
+ Given 1->1->2, return 1->2.
+ Given 1->1->2->3->3, return 1->2->3.
+ */
+
+import Foundation
+
+class Solution83 {
+    
+    func deleteDuplicates(_ head: ListNode?) -> ListNode? {
+        let dummy = ListNode(Int.max)
+        var previousOfDummy: ListNode? = dummy
+        var previousOfHead: ListNode? = head
+        
+        while previousOfHead != nil {
+            if previousOfDummy!.val != previousOfHead!.val {
+                previousOfDummy?.next = ListNode(previousOfHead!.val)
+                previousOfDummy = previousOfDummy?.next
+            }
+            previousOfHead = previousOfHead?.next
+        }
+        return dummy.next
+    }
+    
+    func run() {
+        let listNode = ListNode(array: [1, 1, 2])
+        _ = deleteDuplicates(listNode)
+    }
+}
--- LeetcodeSolution/ViewController.swift
+++ LeetcodeSolution/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         
-        let solution = Solution86()
+        let solution = Solution83()
         solution.run()
     }
 }
