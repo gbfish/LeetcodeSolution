a0d0d65a86384bf08ad0b5f45c5529c2038f3629
David Yu <equalbean@gmail.com>
2018-03-02 16:28:56 -0800

    finish 144 and clean up

---
+++ LeetcodeSolution/Tree/BinaryTreeTraversal/144. Binary Tree Preorder Traversal.swift
@@ -0,0 +1,43 @@
+/*
+ 144. Binary Tree Preorder Traversal
+ 
+ Given a binary tree, return the preorder traversal of its nodes' values.
+ 
+ For example:
+ Given binary tree [1,null,2,3],
+ 1
+ \
+ 2
+ /
+ 3
+ return [1,2,3].
+ */
+
+import Foundation
+
+struct Solution144 {
+    
+    func preorderTraversal(_ root: TreeNode?) -> [Int] {
+        var stack = [TreeNode]()
+        var array = [Int]()
+        if root != nil {
+            stack.append(root!)
+            while stack.count > 0 {
+                let last = stack.removeLast()
+                array.append(last.val)
+                if let right = last.right {
+                    stack.append(right)
+                }
+                if let left = last.left {
+                    stack.append(left)
+                }
+            }
+        }
+        return array
+    }
+    
+    func run() {
+        let root = TreeNode(5)
+        _ = preorderTraversal(root)
+    }
+}
---
+++ LeetcodeSolution/Tree/TreeNode.swift
@@ -0,0 +1,23 @@
+//
+//  TreeNode.swift
+//  LeetcodeSolution
+//
+//  Created by David Yu on 2018/3/2.
+//  Copyright © 2018年 David Yu. All rights reserved.
+//
+
+import Foundation
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+    
+    
+}
